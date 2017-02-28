8df7e98e8d8c62d582c41f7cb98fd34255081970
木村俊介 <kimura.shunsuke@nttcom.co.jp>
2017-02-28 13:33:44 +0900

    入力フィールド削除、履歴に変更

--- src/components/pages/BookPage.vue
+++ src/components/pages/BookPage.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="bookPage container">
     <ranking></ranking>
-    <book-list v-on:selected="showInputBook($event)"></book-list>
-    <input-book v-on:cancel="isActive =false" v-bind:book="book" v-show="isActive"></input-book>
+    <book-list v-on:selected="book = $event"></book-list>
+    <book-history v-bind:isbn="book.isbn" v-show="Object.keys(book).length !== 0"></book-history>
     <advertisement></advertisement>
   </div>
 </template>
 
 <script>
-import InputBook from '@/components/parts/InputBook'
 import BookList from '@/components/parts/BookList'
+import BookHistory from '@/components/parts/BookHistory'
 import Advertisement from '@/components/parts/Advertisement'
 import Ranking from '@/components/parts/Ranking'
 
@@ -17,16 +17,9 @@ export default {
   name: 'bookPage',
   data () {
     return {
-      book: {},
-      isActive: false
+      book: {}
     }
   },
-  methods: {
-    showInputBook (book) {
-      this.isActive = true
-      this.book = book
-    }
-  },
-  components: {BookList, Advertisement, Ranking, InputBook}
+  components: {BookList, Advertisement, Ranking, BookHistory}
 }
 </script>
---
+++ src/components/parts/BookHistory.vue
@@ -0,0 +1,51 @@
+<template>
+
+<div class="bookHisotry">
+  <h1>貸し出し履歴</h1>
+  isbn :{{ isbn  }}
+  <ul>
+    <li>xxx さんが借りています</li>
+    <li>xxx さんが借りたことがあります</li>
+    <li>xxx さんが借りたことがあります</li>
+  </ul>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'bookHistory',
+  props: [ 'isbn' ],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+.bookHistory {
+  animation: fadeIn 0.3s ease 0s 1 normal;
+  -webkit-animation: fadeIn 0.3s ease 0s 1 normal;
+}
+
+@keyframes fadeIn {
+    0% {opacity: 0}
+    100% {opacity: 1}
+}
+
+@-webkit-keyframes fadeIn {
+    0% {opacity: 0}
+    100% {opacity: 1}
+}
+
+h1 {
+  font-size: large;
+}
+.right {
+  float:right;
+}
+.glyphicon {
+  margin-left:1%;
+}
+</style>
